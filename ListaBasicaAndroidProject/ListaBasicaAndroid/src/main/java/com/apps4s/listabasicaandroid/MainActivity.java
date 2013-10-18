50bcc770cc073c0115742f9d515fea8d9e2a3ab7
Henry AT <yiperu@gmail.com>
2013-10-18 15:38:22 -0300

    Programacion del la lista con un arreglo para que se pueda visualizar en el Layout

--- ListaBasicaAndroidProject/ListaBasicaAndroid/src/main/java/com/apps4s/listabasicaandroid/MainActivity.java
+++ ListaBasicaAndroidProject/ListaBasicaAndroid/src/main/java/com/apps4s/listabasicaandroid/MainActivity.java
@@ -3,13 +3,30 @@ package com.apps4s.listabasicaandroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+        private  String cursos[] = {"Flash","iPhone","BlackBerry","Android","WindowsPhone","Sansumg"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lista = (ListView) findViewById(R.id.lista);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, cursos);
+        lista.setAdapter(adaptador);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Codigo Aqui
+            }
+        });
+
     }
 
 
